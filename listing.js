abd9fe1b4bef882af43e896871302d197ea714b3
Virajkandikatla <160853466+Virajkandikatla@users.noreply.github.com>
2025-02-13 10:39:31 +0530

    Routes

--- listing.js
+++ listing.js
@@ -1,104 +1,55 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const listingSchema = new Schema({
-//     title: {
-//         type: String,
-//         required: true,
-//     } ,
-//     description: String,
-//     image:
-//     {
-//         type : String,
-//         default :
-//     " https://media.istockphoto.com/id/182730617/photo/varkala-beach.webp?a=1&b=1&s=612x612&w=0&k=20&c=KOo5jAjXlO-8_YqfmrCgoxuzjqPidd_V9j_HIopbuoM=",
-
-//         set : (v) => v === "" ? " https://media.istockphoto.com/id/182730617/photo/varkala-beach.webp?a=1&b=1&s=612x612&w=0&k=20&c=KOo5jAjXlO-8_YqfmrCgoxuzjqPidd_V9j_HIopbuoM= " : v,
-//     } ,
-//     price: Number,
-//     location: String,
-//     country: String,
-// });
-
-// const Listing = mongoose.model("Listing", listingSchema);
-// module.exports = Listing;
-
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// const listingSchema = new Schema({
-//     title: {
-//         type: String,
-//         required: true,
-//     } ,
-//     description: String,
-//     image:
-//     {
-//        filename:String,
-//        url:{
-
-//              type : String,
-//         default :
-//     " https://media.istockphoto.com/id/182730617/photo/varkala-beach.webp?a=1&b=1&s=612x612&w=0&k=20&c=KOo5jAjXlO-8_YqfmrCgoxuzjqPidd_V9j_HIopbuoM=",
-
-//         set : (v) => v === "" ? " https://media.istockphoto.com/id/182730617/photo/varkala-beach.webp?a=1&b=1&s=612x612&w=0&k=20&c=KOo5jAjXlO-8_YqfmrCgoxuzjqPidd_V9j_HIopbuoM= " : v,
-//         }
-//     } ,
-//     price: Number,
-//     location: String,
-//     country: String,
-// });
-
-// const Listing = mongoose.model("Listing", listingSchema);
-// module.exports = Listing;
-
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const Review = require("./review.js");
-
-const listingSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: String,
-  image: {
-    url: String,
-    filename: String,
-  },
-  price: Number,
-  location: String,
-  country: String,
-  reviews: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Review",
-    },
-  ],
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-
-  geometry:{
-    type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ['Point'], // 'location.type' must be 'Point'
-      required: true
-    },
-    coordinates: {
-      type: [Number],
-      required: true
-    }
-  }
-});
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controllers/listings");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 
-listingSchema.post("findOneAndDelete", async (listing) => {
-  if (listing) {
-    await Review.deleteMany({ _id: { $in: listing.reviews } });
-  }
-});
 
-const Listing = mongoose.model("Listing", listingSchema);
-module.exports = Listing;
+router
+
+  .route("/")
+  // Index route
+  .get(wrapAsync(listingController.index))
+  // Create route
+  .post(
+    isLoggedIn,
+
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
+
+
+
+// New route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+router
+  .route("/:id")
+  //Show route
+  .get(wrapAsync(listingController.showListing))
+  // Update route
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image][url]"),
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  // Delete route
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+// Edit route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.renderEditForm)
+);
+
+module.exports = router;
